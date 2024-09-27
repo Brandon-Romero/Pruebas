d74ff14fc573732df470e60c3b77e95d13945686
Brandon Romero <84001272+Brandon-Romero@users.noreply.github.com>
2024-09-26 19:26:33 -0500

    Add files via upload

---
+++ src/main/java/co/edu/uptc/animals_rest/models/CategoryCount.java
@@ -0,0 +1,31 @@
+package co.edu.uptc.animals_rest.models;
+
+public class CategoryCount {
+
+    private String category;
+    private int number;
+
+    // Constructor
+    public CategoryCount(String category, int number) {
+        this.category = category;
+        this.number = number;
+    }
+
+    // Getters y Setters
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    
+}
